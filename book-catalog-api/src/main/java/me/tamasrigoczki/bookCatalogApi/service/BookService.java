98e475e32d1f29e233e5eef8b139e54d248cb8af
Tamas Rigoczki <rigoczkitamas@gmail.com>
2022-07-28 07:51:19 +0200

    Added BookService

---
+++ book-catalog-api/src/main/java/me/tamasrigoczki/bookCatalogApi/service/BookService.java
@@ -0,0 +1,21 @@
+package me.tamasrigoczki.bookCatalogApi.service;
+
+import lombok.AllArgsConstructor;
+import me.tamasrigoczki.bookCatalogApi.model.entity.Book;
+import me.tamasrigoczki.bookCatalogApi.repository.BookRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void addBook(Book book) {
+        if (bookRepository.existsById(book.getId())) {
+            throw new IllegalArgumentException(book.getId());
+        } else {
+            bookRepository.save(book);
+        }
+    }
+}
