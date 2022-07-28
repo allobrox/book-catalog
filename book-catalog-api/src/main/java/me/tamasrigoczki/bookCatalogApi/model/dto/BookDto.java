99cd544c9237cffea5986d2b8a9b6158a5e6e58e
Tamas Rigoczki <rigoczkitamas@gmail.com>
2022-07-28 07:40:14 +0200

    Added BookDto

---
+++ book-catalog-api/src/main/java/me/tamasrigoczki/bookCatalogApi/model/dto/BookDto.java
@@ -0,0 +1,23 @@
+package me.tamasrigoczki.bookCatalogApi.model.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class BookDto {
+
+    @NotBlank(message = "ID is mandatory")
+    private String id;
+    @NotBlank(message = "Author is mandatory")
+    private String author;
+    @Min(value = 1,message = "Pages must be greater than 0")
+    private int pages;
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+}
