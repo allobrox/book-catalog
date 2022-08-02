c141a330dca3885b261cc76f4813e4d3858160ea
Tamas Rigoczki <rigoczkitamas@gmail.com>
2022-08-02 20:06:40 +0200

    Added BookCollection entity

---
+++ book-catalog-api/src/main/java/me/tamasrigoczki/bookCatalogApi/model/entity/BookCollection.java
@@ -0,0 +1,19 @@
+package me.tamasrigoczki.bookCatalogApi.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class BookCollection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+}
